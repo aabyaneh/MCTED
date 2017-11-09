be13efc0e5dcc2a387d1c7dafd877c65b8986a5e
aabyaneh <abyaneh.alireza@gmail.com>
2017-11-09 16:08:25 +0100

    Added serial and parallel shared variables

---
+++ src/zhsh/zh_sh.h
@@ -0,0 +1,18 @@
+int del_cost = 1;
+int ins_cost = 1;
+int up_cost = 1;
+
+int tree1_size;
+int tree2_size;
+
+int leaves_cnt1;
+int leaves_cnt2;
+
+int* tree1_postorder;
+int* tree2_postorder;
+
+int* l1;
+int* l2;
+
+int* kr1;
+int* kr2;
